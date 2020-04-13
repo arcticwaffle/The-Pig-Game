5612355d765cffeacb14de639c754234108fe97d
Katarzyna <ban.kat.96@hotmail.com>
2020-04-13 16:26:07 +0200

    add fucntionality to btnHold and determinate who is the winner

--- app.js
+++ app.js
@@ -8,10 +8,13 @@ GAME RULES:
 */
 
 const btnRoll = document.querySelector('.btn-roll');
-const scores = [0,1];
+const btnHold = document.querySelector('.btn-hold');
+const diceImg = document.querySelector('.dice');
+const scores = [0,0];
 
 let roundScore = 0;
 let activePlayer = 0;
+
 
 document.getElementById('score-0').textContent = '0';
 document.getElementById('score-1').textContent = '0';
@@ -19,13 +22,51 @@ document.getElementById('current-0').textContent = '0';
 document.getElementById('current-1').textContent = '0';
 
 
-// document.querySelector(`#current-${activePlayer}`).textContent = dice;
+
 // document.querySelector(`#current-${activePlayer}`).innerHTML = `<em>${dice}</em>`;
 
 btnRoll.addEventListener('click', () => {
-    const diceImg = document.querySelector('.dice');
     const dice = Math.floor(Math.random() * 6) + 1;
     
     diceImg.style.display = 'block';
     diceImg.src = `dice-${dice}.png`;
-})
+
+    // Update the round score if the rolled number was not a 1
+    if (dice !== 1) {
+        roundScore += dice;
+        document.querySelector(`#current-${activePlayer}`).textContent = roundScore;
+    } else {
+        // Next player
+        nextPlayer();
+    }
+});
+
+btnHold.addEventListener('click', () => {
+    scores[activePlayer] += roundScore;
+
+    document.querySelector(`#score-${activePlayer}`).textContent = scores[activePlayer];
+
+
+    if(scores[activePlayer] >= 100) {
+        document.querySelector(`#name-${activePlayer}`).textContent = 'Winner';
+        diceImg.style.display = 'none';
+
+        document.querySelector(`.player-${activePlayer}-panel`).classList.add('winner');
+        document.querySelector(`.player-${activePlayer}-panel`).classList.remove('active');
+    } else {
+        nextPlayer();
+    }
+});
+
+function nextPlayer() {
+    activePlayer === 0 ? activePlayer = 1 : activePlayer = 0 ;
+        roundScore = 0;
+
+        document.getElementById('current-0').textContent = '0';
+        document.getElementById('current-1').textContent = '0';
+
+        document.querySelector('.player-0-panel').classList.toggle('active');
+        document.querySelector('.player-1-panel').classList.toggle('active');
+
+        diceImg.style.display = 'none';
+}
