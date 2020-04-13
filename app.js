d1d81218bb826f8f248fdee7a98e324177cf005e
Katarzyna <ban.kat.96@hotmail.com>
2020-04-13 17:21:34 +0200

    create initi function and implemented them

--- app.js
+++ app.js
@@ -7,21 +7,14 @@ GAME RULES:
 - The first player to reach 100 points on GLOBAL score wins the game
 */
 
+let scores, activePlayer, roundScore;
+
 const btnRoll = document.querySelector('.btn-roll');
 const btnHold = document.querySelector('.btn-hold');
+const btnNew = document.querySelector('.btn-new');
 const diceImg = document.querySelector('.dice');
-const scores = [0,0];
 
-let roundScore = 0;
-let activePlayer = 0;
-
-
-document.getElementById('score-0').textContent = '0';
-document.getElementById('score-1').textContent = '0';
-document.getElementById('current-0').textContent = '0';
-document.getElementById('current-1').textContent = '0';
-
-
+init();
 
 // document.querySelector(`#current-${activePlayer}`).innerHTML = `<em>${dice}</em>`;
 
@@ -58,6 +51,30 @@ btnHold.addEventListener('click', () => {
     }
 });
 
+btnNew.addEventListener('click', init);
+
+function init() {
+    scores = [0,0];
+    activePlayer = 0;
+    roundScore = 0;
+
+    document.getElementById('score-0').textContent = '0';
+    document.getElementById('score-1').textContent = '0';
+    document.getElementById('current-0').textContent = '0';
+    document.getElementById('current-1').textContent = '0';
+
+    document.getElementById('name-0').textContent = 'Player 1';
+    document.getElementById('name-1').textContent = 'Player 2';
+
+    document.querySelector(".player-0-panel").classList.remove('winner');
+    document.querySelector(".player-1-panel").classList.remove('winner');
+    document.querySelector(".player-0-panel").classList.remove('active');
+    document.querySelector(".player-1-panel").classList.remove('active');
+    document.querySelector(".player-0-panel").classList.add('active');
+
+
+};
+
 function nextPlayer() {
     activePlayer === 0 ? activePlayer = 1 : activePlayer = 0 ;
         roundScore = 0;
@@ -69,4 +86,4 @@ function nextPlayer() {
         document.querySelector('.player-1-panel').classList.toggle('active');
 
         diceImg.style.display = 'none';
-}
+};
