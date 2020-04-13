0c7e0f31c99a2d25fa98638f8f70bd70bae4955e
Katarzyna <ban.kat.96@hotmail.com>
2020-04-13 14:51:17 +0200

    Declarated basic variables, change style in CSS

--- app.js
+++ app.js
@@ -6,3 +6,12 @@ GAME RULES:
 - The player can choose to 'Hold', which means that his ROUND score gets added to his GLBAL score. After that, it's the next player's turn
 - The first player to reach 100 points on GLOBAL score wins the game
 */
+
+const scores = [0,1];
+let roundScore = 0;
+let activePlayer = 0;
+
+const dice = Math.floor(Math.random() * 6) + 1;
+
+document.querySelector(`#current-${activePlayer}`).textContent = dice;
+// document.querySelector(`#current-${activePlayer}`).innerHTML = `<em>${dice}</em>`;
